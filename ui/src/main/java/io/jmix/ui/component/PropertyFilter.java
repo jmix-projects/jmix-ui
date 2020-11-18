09c9338255e3731e7708493d098442eaa7676fd5
Gleb Gorelov <gorelov@haulmont.com>
2020-11-18 12:10:02 +0400

    Refactor PropertyFilter UI component (171)

--- ui/src/main/java/io/jmix/ui/component/PropertyFilter.java
+++ ui/src/main/java/io/jmix/ui/component/PropertyFilter.java
@@ -22,8 +22,8 @@ import io.jmix.ui.model.DataLoader;
 /**
  * PropertyFilter is a UI component used for filtering entities returned by the {@link DataLoader}. The component is
  * related to entity property and can automatically render proper layout for setting a condition value. In general case
- * a PropertyFilter layout contains a label with entity property caption, operation label (=, contains, &#62;, etc.) and
- * a field for editing a property value.
+ * a PropertyFilter layout contains a label with entity property caption, operation label or selector (=, contains,
+ * &#62;, etc.) and a field for editing a property value.
  */
 public interface PropertyFilter<V> extends Component, Component.BelongToFrame, HasValue<V>,
         Component.HasCaption, Component.HasIcon, Component.Focusable, Component.Editable,
@@ -36,6 +36,11 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
      */
     DataLoader getDataLoader();
 
+    /**
+     * Sets a {@link DataLoader} related to the current PropertyFilter.
+     *
+     * @param dataLoader a {@link DataLoader} to set
+     */
     void setDataLoader(DataLoader dataLoader);
 
     /**
@@ -43,6 +48,11 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
      */
     String getProperty();
 
+    /**
+     * Sets related entity property name.
+     *
+     * @param property entity property name
+     */
     void setProperty(String property);
 
     /**
@@ -50,6 +60,11 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
      */
     Operation getOperation();
 
+    /**
+     * Sets a filtering operation.
+     *
+     * @param operation a filtering operation
+     */
     void setOperation(Operation operation);
 
     /**
@@ -57,20 +72,52 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
      */
     String getParameterName();
 
+    /**
+     * Sets the name of the associated query parameter name.
+     *
+     * @param parameterName a name of the associated query parameter name
+     */
     void setParameterName(String parameterName);
 
+    /**
+     * @return a field for editing a property value
+     */
     HasValue<V> getValueComponent();
 
+    /**
+     * Sets the field for editing a property value.
+     *
+     * @param valueComponent a field for editing a property value
+     */
     void setValueComponent(HasValue<V> valueComponent);
 
+    /**
+     * @return whether an operation selector is visible.
+     */
     boolean isOperationEditable();
 
+    /**
+     * Sets whether an operation selector is visible.
+     *
+     * @param operationEditable whether an operation selector is visible
+     */
     void setOperationEditable(boolean operationEditable);
 
+    /**
+     * @return a caption width value in {@link #getWidthSizeUnit()}
+     */
     float getCaptionWidth();
 
+    /**
+     * @return units used in the caption width property
+     */
     SizeUnit getCaptionWidthSizeUnit();
 
+    /**
+     * Sets the caption width.
+     *
+     * @param captionWidth a string width representation
+     */
     void setCaptionWidth(String captionWidth);
 
     /**
@@ -79,13 +126,23 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
     PropertyCondition getPropertyCondition();
 
     /**
-     * @return true if the filter should be automatically applied to the {@link DataLoader} when the value component
-     * value is changed
+     * @return {@code true} if the filter should be automatically applied to the
+     * {@link DataLoader} when the value component value is changed
      */
     boolean isAutoApply();
 
+    /**
+     * Sets whether the filter should be automatically applied to the {@link DataLoader}
+     * when the value component value is changed.
+     *
+     * @param autoApply {@code true} if the filter should be automatically applied to the
+     *                  {@link DataLoader} when the value component value is changed
+     */
     void setAutoApply(boolean autoApply);
 
+    /**
+     * Operation representing corresponding filtering condition.
+     */
     enum Operation {
         EQUAL(Type.VALUE),
         NOT_EQUAL(Type.VALUE),
@@ -114,10 +171,33 @@ public interface PropertyFilter<V> extends Component, Component.BelongToFrame, H
             return type;
         }
 
+        /**
+         * Operation type representing the required field type for editing
+         * a property value with the given operation.
+         */
         public enum Type {
+
+            /**
+             * Requires a field suitable for editing a property value, e.g.
+             * {@link TextField} for String, {@link ComboBox} for enum.
+             */
             VALUE,
+
+            /**
+             * Requires a field suitable for choosing unary value, e.g. true/false, YES/NO.
+             */
             UNARY,
+
+            /**
+             * Requires a field suitable for selecting multiple values of
+             * the same type as the property value.
+             */
             LIST,
+
+            /**
+             * Requires a field suitable for selecting a range of values of
+             * the same type as the property value.
+             */
             INTERVAL
         }
     }
